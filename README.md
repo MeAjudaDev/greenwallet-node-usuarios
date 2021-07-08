b6781c63c252e11af827292b5c49c67ae3841b80
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-07-08 18:11:34 -0300

    Delete old packages

--- README.md
+++
@@ -1,1 +0,0 @@
-# greenwallet-node-usuarios
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.json());
-app.listen(3333, () => {
-  console.log("Server is listening");
-});
