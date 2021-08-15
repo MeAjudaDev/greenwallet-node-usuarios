d5d6fb3364bf3cc0dd7d7bce82725a83d69822f1
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-08-15 10:17:25 -0300

    chore: create endpoint to activate account

--- src/repositories/UsersRepository.js
+++ src/repositories/UsersRepository.js
@@ -1,14 +1,33 @@
 const connect = require('../database');
 
 exports.create = async ({ name, email, password, activation_code }) => {
-    const conn = await connect();
-    const sql = 'INSERT INTO users(name, email, password, activation_code) VALUES (?, ?, ?, ?)';
-    const values = [
-        name, 
-        email, 
-        password, 
-        activation_code
-    ];
+  const conn = await connect();
+  const sql = 'INSERT INTO users(name, email, password, activation_code) VALUES (?, ?, ?, ?)';
+  const values = [
+    name,
+    email,
+    password,
+    activation_code
+  ];
 
-    return await conn.query(sql, values);
+  return await conn.query(sql, values);
+}
+
+exports.findUserByEmail = async (email) => {
+  const conn = await connect();
+  const sql = 'SELECT name, email, activation_code, state FROM users WHERE email = ?';
+  const values = [email];
+
+  return await conn.query(sql, values);
+}
+
+exports.updateStateColumn = async (email, state) => {
+  const conn = await connect();
+  const sql = "UPDATE users SET state = ? WHERE email= ?";
+  const values = [
+    state,
+    email
+  ];
+
+  return await conn.query(sql, values);
 }
